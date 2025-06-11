d3b9613b3064cb11fe75fcc32addc88e82e8e874
TrmitaDnamita <franelasd@gmail.com>
2025-06-11 01:11:06 -0300

    done module1

--- Course-Practice/module-1/ascii-table.js
+++ Course-Practice/module-1/ascii-table.js
@@ -1,3 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const ASCII_LENGTH = 256;
+	const ROW_LENGTH = 8;
+	
+	const tableLength = Math.floor(ASCII_LENGTH / ROW_LENGTH);
+	const asciiTableRows = [];
+	
+	asciiTableRows.push(`
+		<table>
+			<caption>This is a table of all the ASCII characters</caption>
+	`);
+	
+	asciiTableRows.push(`<thead><tr>`);
+	for (let i = 0; i < ROW_LENGTH; i++) {
+		asciiTableRows.push(`<th>Number</th><th>Character</th>`);
+	}
+	asciiTableRows.push(`</tr></thead>`);
+	
+	let col = [];
+	
+	for (let i = 0; i <= tableLength; i++) {
+		for (let j = 0; j <= tableLength; j++) {
+			let actualPos = j + (tableLength * j) + i
+			col[j] = actualPos < ASCII_LENGTH ? actualPos : NaN;
+		}
+		
+		asciiTableRows.push(`<tr>`);
+		for (let k = 0; k < ROW_LENGTH; k++) {
+			asciiTableRows.push(`<td>${col[k]}</td><td>${String.fromCharCode(col[k])}</td>`);
+		}
+		asciiTableRows.push(`<tr>`);
+	}
+	
+	asciiTableRows.push(`</table>`);
+	document.getElementById("ascii-output").innerHTML += asciiTableRows.join("");
+});
+
+/*
 const TAU = 6.283185307179586476925286766559005768394338798750211641949889184615;
 
 let myFive = 5;
@@ -63,36 +101,4 @@ _main = () => {
 }
 
 console.table([null, _main(), NaN, false, true, "Hello World!"]);
-
-document.addEventListener("DOMContentLoaded", () => {
-	const ASCII_LENGTH = 256;
-	const ROW_LENGTH = 5;
-	
-	const tableLength = Math.floor(ASCII_LENGTH / ROW_LENGTH);
-	const asciiTableRows = [];
-	
-	asciiTableRows.push(`<caption>This is a table of all the ASCII characters</caption>`);
-	
-	asciiTableRows.push(`<thead><tr>`);
-	for (let i = 0; i < ROW_LENGTH; i++) {
-		asciiTableRows.push(`<th>Number</th><th>Character</th>`);
-	}
-	asciiTableRows.push(`</tr></thead>`);
-	
-	let col = [];
-	
-	for (let i = 0; i <= tableLength; i++) {
-		for (let j = 0; j <= tableLength; j++) {
-			let actualPos = j + (tableLength * j) + i
-			col[j] = actualPos < ASCII_LENGTH ? actualPos : NaN;
-		}
-		
-		asciiTableRows.push(`<tr>`);
-		for (let k = 0; k < ROW_LENGTH; k++) {
-			asciiTableRows.push(`<td>${col[k]}</td><td>${String.fromCharCode(col[k])}</td>`);
-		}
-		asciiTableRows.push(`<tr>`);
-	}
-	
-	document.getElementById("ascii-output").innerHTML = asciiTableRows.join("");
-});
+*/
